93474484b5385e5dec67fa84d88a32c8d26e26c4
Kirill Zhitko <kirillzhitko@gmail.com>
2024-11-05 11:59:03 +0300

    REFACTOR: launch only nodes

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-GOOS=windows GOARCH=amd64 go build -o ./build/reLauncherX64.exe
-GOOS=windows GOARCH=386 go build -o ./build/reLauncherX32.exe
+GOOS=windows GOARCH=amd64 go build -o ./build/denode-launcher-windows-amd64.exe
 
-GOOS=linux GOARCH=amd64 go build -o ./build/reLauncherX64Linux
-GOOS=linux GOARCH=amd64 go build -o ./build/reLauncherX32Linux
+GOOS=linux GOARCH=amd64 go build -o ./build/denode-launcher-linux-amd64
+GOOS=linux GOARCH=arm go build -o ./build/denode-launcher-linux-armv6
 
-GOOS=darwin GOARCH=amd64 go build -o ./build/reLauncherMacX64
-GOOS=darwin GOARCH=arm64 go build -o ./build/reLauncherMacA64
+GOOS=darwin GOARCH=amd64 go build -o ./build/denode-launcher-darwin-amd64
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"time"
 )
 
@@ -13,36 +14,43 @@ func main() {
 	var reLaunch = 0
 	defer logger.Printf("Total relaunch`s %d\n", reLaunch)
 
-	logger.Print("reLauncher start")
-
-	args := os.Args[1:]
-	if len(args) == 0 {
-		logger.Fatalf("The first argument must specify the executable file.")
-	}
+	exeFileName := GetExeFileName()
 
 	for {
 		reLaunch++
-		cmd := exec.Command(args[0], args[1:]...)
+		cmd := exec.Command(exeFileName, os.Args[1:]...)
 		cmd.Stdout = os.Stdout
 		cmd.Stdin = os.Stdin
 		cmd.Stderr = os.Stderr
 
 		err := cmd.Start()
 		if err != nil {
-			logger.Printf("%d. Command(%s) start with error=%s\n", reLaunch, args[0], err)
+			logger.Printf("%d. Start node with error=%s\n", reLaunch, err)
 			time.Sleep(time.Second * 5)
 			continue
 		}
 		pid := cmd.Process.Pid
-		logger.Printf("%d. Process(%d) started\n", reLaunch, pid)
+		logger.Printf("%d. Node(pid: %d) started\n", reLaunch, pid)
 
 		err = cmd.Wait()
 		if err != nil {
-			logger.Printf("%d. Process(%d) exited with err=%s\n", reLaunch, pid, err)
+			logger.Printf("%d. Node(pid: %d) exited with err=%s\n", reLaunch, pid, err)
 		} else {
-			logger.Printf("%d. Process(%d) exited\n", reLaunch, pid)
+			logger.Printf("%d. Node(pid: %d) exited\n", reLaunch, pid)
 		}
 
 		time.Sleep(time.Second)
 	}
+}
+
+func GetExeFileName() string {
+	goos := runtime.GOOS
+	goarch := runtime.GOARCH
+
+	var postfix = ""
+	if goos == "windows" {
+		postfix = ".exe"
+	}
+
+	return "./denode-" + goos + "-" + goarch + postfix
 }
